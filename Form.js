50e1f5c878c1f36c2590fd7aa63361fc36f6292c
leo312002 <142529075+leo312002@users.noreply.github.com>
2024-09-29 11:11:19 +0200

    Add files via upload

--- Form.js
+++ Form.js
@@ -16,14 +16,40 @@ function addFriend(){
 
     newFormFriend.id = `formFriend${friendAdded}`;
 
-    newFormFriend.innerHTML = `<p id="friendNumber" class="mt-4 mb-3 fw-bold fs-5">Friend number ${friendAdded}:</p>`;
-    newFormFriend.innerHTML += '<div class="row-sm mb-4 mt-4"><div class="col-lg-3 col-md-2"></div><div class="col-lg-3 col-md-4 col-sm-6 mx-5 mb-3"><label for="validationDefault01" class="form-label">Nome</label><input type="text" class="form-control" id="validationDefault01" value="" required></div><div class="col-lg-3 col-md-4 col-sm-6 mx-5"><label for="validationDefault02" class="form-label">Cognome</label><input type="text" class="form-control" id="validationDefault02" value="" required></div><div class="col-lg-3 col-md-2"></div></div><div id="phoneNumber" class="row-sm mb-5"><div class="col-md-4 col-2"></div><div class="col-md-4 col-sm-8 mx-5"><label for="validationDefault03" class="form-label">Telefono</label><div class="input-group"><span class="input-group-text" id="inputGroupPrepend3">+39</span><input type="tel" class="form-control" id="validationDefault03" aria-describedby="inputGroupPrepend3" required></div></div><div class="col-md-4 col-2"></div></div>';
-    
+    newFormFriend.innerHTML = `<p id="friendNumber${friendAdded}" class="mt-5 mb-3 fw-bold fs-5">Friend number ${friendAdded}:</p>`;
+    var a = `<div id="row-nome-cognome${friendAdded}" class="row mb-4 mt-4">`+
+              '<div class="col-lg-3 col-md-2"></div>'+
+              `<div id="Nome${friendAdded}" class="col-lg-3 col-md-4 col-sm-6 mb-3">`+
+                '<label for="validationDefault01" class="form-label">Nome</label>'+
+                '<input type="text" class="form-control" id="validationDefault01" value="" required>'+
+              '</div>'+
+              `<div id="Cognome${friendAdded}" class="col-lg-3 col-md-4 col-sm-6">`+
+                '<label for="validationDefault02" class="form-label">Cognome</label>'+
+                '<input type="text" class="form-control" id="validationDefault02" value="" required>'+
+              '</div>'+
+              '<div class="col-lg-3 col-md-2"></div>'+
+            '</div>'+
+            `<div id="phoneNumber${friendAdded}" class="row mb-5">`+
+              '<div class="col-md-4 col-2"></div>'+
+              `<div id="Phone${friendAdded}" class="col-md-4 col-sm-8">`+
+                '<label for="validationDefault03" class="form-label">Telefono</label>'+
+                '<div class="input-group">'+
+                  '<span class="input-group-text" id="inputGroupPrepend3">+39</span>'+
+                  '<input type="tel" class="form-control" id="validationDefault03" aria-describedby="inputGroupPrepend3" required>'+
+                '</div>'+
+              '</div>'+
+              '<div class="col-md-4 col-2"></div>'+
+            '</div>';
+
+    newFormFriend.innerHTML += a;
+
     document.getElementById("formFriend").append(newFormFriend);
 
     if (friendAdded === 1) {
         document.getElementById("btn-removeFriend").style.display = "inline-block";
     }
+
+    sizeStyle();
 }
 
 function removeFriend() {
@@ -43,12 +69,26 @@ function sizeStyle(){
     document.getElementById('Cognome').classList.add('mx-5');
     document.getElementById("phoneNumber").classList.remove("row");
     document.getElementById('Phone').classList.add('mx-5');
+    for(var i = friendAdded; i > 0; i--){
+      document.getElementById(`row-nome-cognome${i}`).classList.remove("row");
+      document.getElementById(`Nome${i}`).classList.add('mx-5');
+      document.getElementById(`Cognome${i}`).classList.add('mx-5');
+      document.getElementById(`phoneNumber${i}`).classList.remove("row");
+      document.getElementById(`Phone${i}`).classList.add('mx-5');
+    }
   }else if(window.innerWidth >= 576){
     document.getElementById("row-nome-cognome").classList.add("row");
     document.getElementById('Nome').classList.remove('mx-5');
     document.getElementById('Cognome').classList.remove('mx-5');
     document.getElementById("phoneNumber").classList.add("row");
     document.getElementById('Phone').classList.remove('mx-5');
+    for(var i = friendAdded; i > 0; i--){
+      document.getElementById(`row-nome-cognome${i}`).classList.add("row");
+      document.getElementById(`Nome${i}`).classList.remove('mx-5');
+      document.getElementById(`Cognome${i}`).classList.remove('mx-5');
+      document.getElementById(`phoneNumber${i}`).classList.add("row");
+      document.getElementById(`Phone${i}`).classList.remove('mx-5');
+    }
   }
 }
 
